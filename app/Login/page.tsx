30f4cac6e2146150c0a189611a0272527bb88b88
yjjeon <devgrxxn@google.com>
2024-02-07 21:50:27 +0900

    Feat: 로그인 페이지 디자인적용

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,12 +1,28 @@
 import Link from "next/link";
+import styles from "./components/login.module.scss";
 
 export default function Login() {
 
   return (
-    <div>
+    <div className={styles.loginWrapper}>
       <h1>
         <Link href={'/'}>경제야 놀자</Link>
       </h1>
+      <div className={styles.logionBox}>
+        <p>로그인</p>
+        <div>
+          <label>아이디</label>
+          <input type="text" />
+          <label>비밀번호</label>
+          <input type="password" />
+          <label>
+            <input type="checkbox" />
+            <span>아이디 기억하기</span>
+          </label>
+        </div>
+        <button type="button">로그인</button>
+        <button type="button">회원가입</button>
+      </div>
     </div>
   );
 };
