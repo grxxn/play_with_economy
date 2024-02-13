a8b468cccfef3d4a25cf846a8c37d8a848dda25c
yjjeon <jcb8965@naver.com>
2024-02-13 23:50:34 +0900

    Fix: 다이어리 상세 페이지 라우터 수정 - 페이지 병합

--- app/diary/components/DiaryCard.tsx
+++ app/diary/components/DiaryCard.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
 import styles from './card.module.scss';
 
 interface DiaryCardType {
@@ -9,9 +12,18 @@ interface DiaryCardType {
 }
 
 export default function DiaryCard({ date, excRattVal, intrRatVal, stcPricVal, oilPricVal }: DiaryCardType) {
+  const router = useRouter();
+
+  const clickDiaryItem = () => {
+    const dateArr = date.substring(0, 10).split('.');
+    const dateParams = dateArr[0] + dateArr[1] + dateArr[2];
+    console.log(dateParams)
+
+    router.push(`/diary/${dateParams}`)
+  }
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={clickDiaryItem}>
       <h3>{date}</h3>
       <p>
         <span>환율</span>
